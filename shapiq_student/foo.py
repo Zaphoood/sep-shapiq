1bab1b4fa02553c281825519d5104a856c89df0b
Mathis Weber <mathis.c.weber@web.de>
2025-06-01 13:57:34 +0200

    chore: add 'from `__future`' import

--- shapiq_student/foo.py
+++ shapiq_student/foo.py
@@ -1,3 +1,6 @@
-class Foo():
+from __future__ import annotations
+
+
+class Foo:
     def bar(self, x: int, y: int) -> int:
         return x + y
--- tests/test_foo.py
+++ tests/test_foo.py
@@ -1,3 +1,5 @@
+from __future__ import annotations
+
 from shapiq_student.foo import Foo
 
 
