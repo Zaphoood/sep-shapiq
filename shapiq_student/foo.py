59f97caaf1f9b9cbe892a964168364d858cf58dc
Mathis Weber <mathis.c.weber@web.de>
2025-06-01 14:06:09 +0200

    feat: add docstrings to dummy class and methods

--- shapiq_student/foo.py
+++ shapiq_student/foo.py
@@ -1,6 +1,11 @@
+"""A sample module that needs a docstring in order for the linter to be happy."""
+
 from __future__ import annotations
 
 
 class Foo:
+    """A sample class to illustrate the project structure and DevOps setup."""
+
     def bar(self, x: int, y: int) -> int:
+        """Sample function that performs addition on integers."""
         return x + y
