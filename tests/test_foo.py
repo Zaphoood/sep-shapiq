473f9b0996aa87537610578230fa0c6fb0412753
Mathis Weber <mathis.c.weber@web.de>
2025-06-02 15:25:04 +0200

    feat: add tests for baz()

--- tests/test_foo.py
+++ tests/test_foo.py
@@ -2,10 +2,15 @@
 
 from __future__ import annotations
 
-from shapiq_student.foo import Foo
+from shapiq_student.foo import Foo, baz
 
 
 def test_bar():
     """Tests that the bar method performs addition."""
     foo = Foo()
     assert foo.bar(2, 3) == 5  # noqa: PLR2004
+
+
+def test_baz():
+    """Tests that the baz function can be called."""
+    baz()
