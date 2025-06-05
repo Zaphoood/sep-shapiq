6025732a50ec3efaf3138114a4a254477d8bda9e
max <m.neuner@campus.lmu.de>
2025-06-05 13:44:16 +0200

    more fixing of variables

--- shapiq_student/knn_explainer.py
+++ shapiq_student/knn_explainer.py
@@ -6,11 +6,13 @@ from typing import TYPE_CHECKING
 
 import numpy as np
 
+from knn_super import KNNExplainer
+
 if TYPE_CHECKING:
     import sklearn.neighbors
 
 
-class KNNClassifierExplainer:
+class KNNClassifierExplainer(KNNExplainer):
     """KNN Classifier Explainer.
 
     For calculating exact shapley values for a KNN Classifier.
@@ -35,51 +37,50 @@ class KNNClassifierExplainer:
         self.data = data
         self.distance_fn = self._euclidean_distance
         self.support_values = None
+        self.testdings = 325
+
+        self.y_test: np.ndarray = None  # TODO(max): extract from model
+        self.X_train: np.ndarray = None  # TODO(max): extract from model
+        self.y_train: np.ndarray = None  # TODO(max): extract from model
+        self.K: int = 5  # TODO(max): extract from model
 
     def _euclidean_distance(self, x1: any, x2: any) -> any:
         return np.linalg.norm(x1 - x2)
 
-    def explain(
-        self,
-        X_test: np.ndarray,
-        y_test: np.ndarray,
-        X_train: np.ndarray,
-        y_train: np.ndarray,
-        K: int = 5,
-    ) -> np.ndarray:
+    def explain(self, X_test: np.ndarray, class_index: int = 1) -> np.ndarray:
         """Compute shapley values for training data.
 
         Parameters:
         - X_test (np.ndarray): Test features, shape (N_test, d).
-        - y_test (np.ndarray): Test labels, shape (N_test,).
-        - X_train (np.ndarray): Training features, shape (N, d).
-        - y_train (np.ndarray): Training labels, shape (N,).
+        - class_index (int): The index of y_test to be explained. Defaults to 1.
 
         Returns:
         - np.ndarray: Shapley values for training data, shape (N,).
         """
-        N = len(X_train)
-        N_test = len(X_test)
-        s = np.zeros(N)
-        self.K = K
+        self.X_test = X_test
+        self.class_index = class_index
+
+        N = len(self.X_train)
+        N_test = len(self.X_test)
+        s = np.zeros(self.N)
 
         for j in range(N_test):
             x_test_j = X_test[j]
-            y_test_j = y_test[j]
+            y_test_j = self.y_test[j]
 
-            distances = [self.distance_fn(x, x_test_j) for x in X_train]
+            distances = [self.distance_fn(x, x_test_j) for x in self.X_train]
             sorted_indices = np.argsort(distances)
 
             s_j = np.zeros(N)
             last_idx = sorted_indices[-1]
-            s_j[last_idx] = int(y_train[last_idx] == y_test_j) / N
+            s_j[last_idx] = int(self.y_train[last_idx] == y_test_j) / N
 
             for i in reversed(range(N - 1)):
                 idx_i = sorted_indices[i]
                 idx_ip1 = sorted_indices[i + 1]
 
-                label_match_i = int(y_train[idx_i] == y_test_j)
-                label_match_ip1 = int(y_train[idx_ip1] == y_test_j)
+                label_match_i = int(self.y_train[idx_i] == y_test_j)
+                label_match_ip1 = int(self.y_train[idx_ip1] == y_test_j)
 
                 term = (label_match_i - label_match_ip1) * min(self.K, i + 1) / (self.K * (i + 1))
                 s_j[idx_i] = s_j[idx_ip1] + term
