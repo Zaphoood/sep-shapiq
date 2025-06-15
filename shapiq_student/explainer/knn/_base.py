5ee6c564685d1776b65373306c20379a0c9d029f
Mathis Weber <mathis.c.weber@web.de>
2025-06-15 17:28:28 +0200

    implement `KNNExplainerBase` class: extract training data from model

--- shapiq_student/explainer/knn/_base.py
+++ shapiq_student/explainer/knn/_base.py
@@ -1,35 +1,52 @@
-"""KNN_Explainer - Wrapper for the Different KNN Explainers."""
+"""Implementation of KNNExplainerBase class."""
 
 from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
+import numpy as np
 from shapiq import Explainer
+from sklearn.utils.validation import check_is_fitted
 
 if TYPE_CHECKING:
-    import numpy as np
     from sklearn.neighbors import KNeighborsClassifier
 
 
 class KNNExplainerBase(Explainer):
-    """KNN Explainer.
+    """Base class for all KNN explainers.
 
-    Used for Extracting the training data from the model.
+    In the constructor, training data and paramater k are extracted from the model.
     """
 
     def __init__(
         self,
         model: KNeighborsClassifier,
     ) -> None:
-        """Initialize the KNN Shapley Calculator.
+        """Initialize the KNNExplainerBase class.
 
-        Parameters:
-        - model: Type of the KNN Classifier to be explained.
+        Args:
+            model: The KNN model to explain. Must be an instance of sklearn.neighbors.KNeighborsClassifier.
+
+        Raises:
+            sklearn.exceptions.NotFittedError: The constructor was called with a model that hasn't been fitted.
         """
+        check_is_fitted(model)
+
         self.model = model
 
-        raise NotImplementedError
+        self.X_train = model._fit_X  # noqa: SLF001
 
-        self.k: int = 5  # to be taken from the model
-        self.X_train: np.ndarray  # to be taken from the model
-        self.y_train: np.ndarray  # to be taken from the model
+        y_train_indices = model._y  # noqa: SLF001
+        y_train_classes = model.classes_
+
+        # TODO(Zaphoood): Consider disallowing y_train being a matrix
+        if y_train_indices.ndim == 1:
+            self.y_train = y_train_classes[y_train_indices]
+        else:
+            n_outputs = y_train_indices.shape[1]
+            self.y_train = np.empty((self.X_train.shape[0], n_outputs))
+
+            for col, (y_current_train_classes, y_current_train_indices) in enumerate(
+                zip(y_train_classes, y_train_indices.T, strict=False)
+            ):
+                self.y_train[:, col] = y_current_train_classes[y_current_train_indices]
