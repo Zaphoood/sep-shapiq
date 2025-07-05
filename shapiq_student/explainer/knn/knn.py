2f16aabe0824ec9517ffb6b1607877db1e816273
Mathis Weber <mathis.c.weber@web.de>
2025-07-05 15:28:07 +0200

    factor out `LookupGame` to separate class

--- shapiq_student/explainer/knn/knn.py
+++ shapiq_student/explainer/knn/knn.py
@@ -7,41 +7,14 @@ from typing import TYPE_CHECKING
 from typing_extensions import override
 
 import numpy as np
-from shapiq import Game, InteractionValues
 
-from shapiq_student.explainer.knn import KNNExplainerBase, interaction_values_from_array
+from .base import KNNExplainerBase, interaction_values_from_array
+from .lookup_game import LookupGame
 
 if TYPE_CHECKING:
     import numpy.typing as npt
+    from shapiq import InteractionValues
     import sklearn.neighbors
-
-
-class LookupGame(Game):
-    """Defines a Game via a dictionary giving the utility function."""
-
-    def __init__(self, n_players: int, utilities: dict[tuple[int, ...], float]) -> None:
-        """Initializes the LookupGame."""
-        self.characteristic_function = utilities
-        super().__init__(
-            n_players=n_players,
-            normalization_value=self.characteristic_function[()],
-        )
-
-    def value_function(self, coalitions: npt.NDArray[np.bool]) -> npt.NDArray[np.floating]:
-        """Defines the worth of a coalition as a lookup in the characteristic function.
-
-        Args:
-            coalitions: A 2D array where each row represents a coalition as a binary
-                vector (1 for present, 0 for absent).
-
-        Returns:
-            A 1D array containing the value of each coalition based on the
-                characteristic function.
-        """
-        output = [
-            self.characteristic_function[tuple(np.where(coalition)[0])] for coalition in coalitions
-        ]
-        return np.array(output)
 
 
 class BruteForceKNNClassifierExplainer(KNNExplainerBase):
