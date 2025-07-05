ad66d9417c0672a645716fe49e2c029d0ee1ad8f
Tanja Mursch <tanjamursch@gmail.com>
2025-07-05 23:12:57 +0200

    fix linter err

--- shapiq_student/explainer/knn/__init__.py
+++ shapiq_student/explainer/knn/__init__.py
@@ -1,7 +1,12 @@
 """Implementation of KNN Explainers."""
 
 from .base import KNNExplainerBase, interaction_values_from_array, interaction_values_to_array
-from .knn import BruteForceKNNClassifierExplainer, KNNClassifierExplainer
+from .knn import (
+    BruteForceKNNClassifierExplainer,
+    KNNClassifierExplainer,
+    TKNNExplainer,
+    TKNNExplainerEfficient,
+)
 
 __all__ = [
     "KNNExplainerBase",
@@ -9,4 +14,6 @@ __all__ = [
     "interaction_values_to_array",
     "BruteForceKNNClassifierExplainer",
     "KNNClassifierExplainer",
+    "TKNNExplainer",
+    "TKNNExplainerEfficient",
 ]
