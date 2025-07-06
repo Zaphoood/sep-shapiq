2ae2a822aee768f88c57c488a9e052c90d7ed6ce
Mathis Weber <mathis.c.weber@web.de>
2025-07-07 00:47:00 +0200

    refactor wknn tests

--- tests/tests_knn/test_wknn.py
+++ tests/tests_knn/test_wknn.py
@@ -2,17 +2,27 @@
 
 from __future__ import annotations
 
-from dataclasses import dataclass
 from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+from dataclasses import dataclass
 
 import numpy as np
 import numpy.typing as npt
 from sklearn.neighbors import KNeighborsClassifier
 
+from shapiq_student.explainer.knn.base import interaction_values_to_array
 from shapiq_student.explainer.knn.wknn import BruteForceWKNNExplainer, WKNNExplainer
 
-if TYPE_CHECKING:
-    from shapiq.interaction_values import InteractionValues
+
+@dataclass
+class Dataset:
+    """Dataset for unit tests that groups X_train, y_train and a validation point x_val."""
+
+    X_train: npt.NDArray[np.floating]
+    y_train: npt.NDArray[np.int64]
+    x_val: npt.NDArray
 
 
 @dataclass
@@ -25,58 +35,68 @@ class WKNNTestCase:
     k: int
     n_bits: int
 
+    @classmethod
+    def from_dataset(cls, dataset: Dataset, k: int, n_bits: int) -> WKNNTestCase:
+        """Converts a Dataset to a WKNNTestCase."""
+        return cls(dataset.X_train, dataset.y_train, dataset.x_val, k=k, n_bits=n_bits)
 
-def test_wknn_random() -> None:
+
+def test_wknn_sv_agrees_with_brute_force() -> None:
     """Tests that the results of WKNNExplainer agree with those of BruteForceWKNNExplainer using randomly generated test cases."""
-    n_test_cases = 3
-    min_training_points = 5
-    max_training_points = 10
+    n_test_cases = 10
+    n_train_min = 5
+    n_train_max = 10
     n_bits = 4
-    k = 3
 
     rng = np.random.default_rng(seed=43)
 
-    for _ in range(n_test_cases):
-        n = int(rng.integers(min_training_points, max_training_points))
-        X_train, y_train = _generate_binary_split_training_data(rng, n)
+    for dataset, k in random_binary_test_datasets(
+        rng, n_test_cases, n_train_min=n_train_min, n_train_max=n_train_max, k_min=3, k_max=5
+    ):
+        _check_wknn_test_case_with_brute_froce(
+            WKNNTestCase.from_dataset(dataset, k=k, n_bits=n_bits)
+        )
+
+
+def random_binary_test_datasets(
+    rng: np.random.Generator,
+    n: int,
+    n_train_min: int,
+    n_train_max: int,
+    k_min: int,
+    k_max: int,
+) -> Iterator[tuple[Dataset, int]]:
+    """Randomly generates binary datasets for testing."""
+    for _ in range(n):
+        n = int(rng.integers(n_train_min, n_train_max, endpoint=True))
+        X_train = rng.normal(size=(n, 2))
+        y_train = rng.integers(0, 2, size=n)
         x_val = rng.normal(size=(1, 2))[0]
 
-        _check_wknn_test_case(WKNNTestCase(X_train, y_train, x_val, k=k, n_bits=n_bits))
+        k = int(rng.integers(k_min, k_max, endpoint=True))
+
+        yield Dataset(X_train=X_train, y_train=y_train, x_val=x_val), k
 
 
-def _check_wknn_test_case(test_case: WKNNTestCase) -> None:
+def _check_wknn_test_case_with_brute_froce(test_case: WKNNTestCase) -> None:
     model = KNeighborsClassifier(n_neighbors=test_case.k, weights="distance")
     model.fit(test_case.X_train, test_case.y_train)
 
     for class_index in range(len(set(test_case.y_train))):
         explainer_wang = WKNNExplainer(model, class_index=class_index, n_bits=test_case.n_bits)
         iv_wang = explainer_wang.explain(test_case.x_val)
-        sv_wang = _interaction_values_to_array(iv_wang)
+        sv_wang = interaction_values_to_array(iv_wang)
 
         explainer_brute = BruteForceWKNNExplainer(
             model,
             class_index=class_index,
-            # Use discretized weights in brute force explainer
+            # Discretize weights in brute force explainer
             n_bits=test_case.n_bits,
         )
         iv_brute = explainer_brute.explain_function(test_case.x_val)
-        sv_brute = _interaction_values_to_array(iv_brute)
+        sv_brute = interaction_values_to_array(iv_brute)
 
         assert np.allclose(sv_brute, sv_wang)
-
-
-def _generate_binary_split_training_data(
-    rng: np.random.Generator,
-    n: int,
-    split_vec: npt.NDArray[np.floating] | None = None,
-) -> tuple[npt.NDArray[np.floating], npt.NDArray[np.int64]]:
-    if split_vec is None:
-        split_vec = np.array([1, 1])
-
-    X = rng.normal(size=(n, 2))
-    y = rng.integers(0, 2, size=n)
-
-    return X, y
 
 
 def test_wknn_discretize_weights():
@@ -117,28 +137,5 @@ def test_wknn_discretize_weights():
     assert explainer._flip_weight_sign(weights_prepared[2]) == weights_prepared[0]
 
 
-def _interaction_values_to_array(shapley_values: InteractionValues) -> npt.NDArray[np.floating]:
-    """Extracts Shapley Values from an ``InteractionValues`` object and returns them in an array, ordered by player index.
-
-    Args:
-        shapley_values: An InteractionValues object with ``max_order==1``
-
-    Returns:
-        An ``np.ndarray`` of shape (n_players,) containing at index i the Shapley value of player i.
-    """
-    if shapley_values.max_order != 1:
-        msg = f"Max order must be 1 but was {shapley_values.max_order}"
-        raise ValueError(msg)
-
-    out = np.zeros((shapley_values.n_players,))
-
-    for coalition, lookup_idx in shapley_values.interaction_lookup.items():
-        if coalition == ():
-            continue
-        out[coalition[0]] = shapley_values.values[lookup_idx]
-
-    return out
-
-
 if __name__ == "__main__":
-    test_wknn_random()
+    test_wknn_sv_agrees_with_brute_force()
