2893b1b9d9d2ecc4bc52ead51d25aa8be4c1b7d0
Mathis Weber <mathis.c.weber@web.de>
2025-07-11 13:18:56 +0200

    test automagic selection of knn explainer subclass

--- tests/tests_knn_explainer/test_common_knn_explainer.py
+++ tests/tests_knn_explainer/test_common_knn_explainer.py
@@ -6,8 +6,11 @@ import numpy as np
 import pytest
 from sklearn.neighbors import KNeighborsClassifier
 
+from shapiq_student.explainer.knn._common_knn import _CommonKNNExplainer
 from shapiq_student.explainer.knn.base import KNNExplainer
 from shapiq_student.explainer.knn.exceptions import UnsupportedKNNWeightsError
+from shapiq_student.explainer.knn.normal_knn import NormalKNNExplainer
+from shapiq_student.explainer.knn.weighted_knn import WeightedKNNExplainer
 
 
 def test_raises_on_unsupported_weights_value():
@@ -23,3 +26,17 @@ def test_raises_on_unsupported_weights_value():
 
     with pytest.raises(UnsupportedKNNWeightsError):
         KNNExplainer(model, class_index=0)
+
+
+def test_select_explainer():
+    """Tests that _CommonKNNExplainer automagically turns into the right explainer class for a given weights parameter."""
+    test_cases = [
+        ("uniform", NormalKNNExplainer),
+        ("distance", WeightedKNNExplainer),
+    ]
+
+    for weights, expected_explainer_class in test_cases:
+        model = KNeighborsClassifier(weights=weights)
+        model.fit(np.array([[0]]), np.array([0]))
+        explainer = _CommonKNNExplainer(model, class_index=0)
+        assert isinstance(explainer, expected_explainer_class)
