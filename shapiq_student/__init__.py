b8b2b73bfafbcf484acc2e6bb39119d7a37a2daa
Mathis Weber <mathis.c.weber@web.de>
2025-07-17 15:48:52 +0200

    delete subset finding stub

--- shapiq_student/__init__.py
+++ shapiq_student/__init__.py
@@ -1,5 +1,1 @@
 """Source code for the shapiq_student package."""
-
-
-def subset_finding(interaction_values: object, max_size: int) -> object:
-    """Stub for subset finding function (docstring required by linter)."""
