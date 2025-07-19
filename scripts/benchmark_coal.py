04b7079f03757ffefa9543d6cf273d5af6f6cb76
Mathis Weber <mathis.c.weber@web.de>
2025-07-19 13:12:13 +0200

    add coalition finding benchmark with precomputed values

--- scripts/benchmark_coal.py
+++ scripts/benchmark_coal.py
@@ -6,6 +6,10 @@ from typing import TYPE_CHECKING
 
 if TYPE_CHECKING:
     from collections.abc import Iterator
+    from typing import Literal
+
+from pathlib import Path
+
 from shapiq import ExactComputer, InteractionValues
 from shapiq.games.benchmark import SOUM
 
@@ -27,8 +31,8 @@ def random_ivs_from_soums(
         random_state += 1
 
 
-def main() -> None:
-    """The main entry point of the script."""
+def benchmark_soums() -> None:
+    """Benchmarks the coalition finding algorithm using Sum of Unanimity Games."""
     n_games = 10
     n_players = 10
     coal_size = 4
@@ -53,6 +57,53 @@ def main() -> None:
         )
         print(f"avg score (min coal): {avg_min_score:.3f}")
         print(f"avg score (max coal): {avg_max_score:.3f}")
+
+
+def load_iv(*, instance: Literal["a", "b", "c"], large: bool) -> InteractionValues:
+    """Load interaction values for a file.
+
+    Args:
+        instance: The instance to load.
+        large: Whether to load the large or medium version of the interaction values.
+            The large version has about 200 players, and the medium version has approximately
+            50 players.
+
+    Returns:
+        The loaded interaction values.
+    """
+    path = Path(__file__).parent.parent / "tests_grading" / "data"
+    size = "large" if large else "medium"
+    path = path / f"iv_{instance}_{size}.pkl"
+
+    iv = InteractionValues.load(path=str(path))
+    return iv
+
+
+def benchmark_precompute() -> None:
+    """Benchmarks the coalition finding algorithm using precomputed games."""
+    coal_sizes = [2, 3, 4, 5]
+    instances: list[Literal["a", "b", "c"]] = ["a", "b", "c"]
+
+    for instance in instances:
+        for coal_size in coal_sizes:
+            print(f"{instance=}")
+            print(f"{coal_size=}")
+
+            iv = load_iv(instance=instance, large=False)
+            print(f"n_players: {iv.n_players}")
+
+            min_score, max_score = benchmark(
+                strategy="best_individuals",
+                coal_size=coal_size,
+                ivs=[iv],
+            )
+            print(f"score (min coal): {min_score:.3f}")
+            print(f"score (max coal): {max_score:.3f}")
+
+
+def main() -> None:
+    """The main entry point of the script."""
+    benchmark_precompute()
 
 
 if __name__ == "__main__":
